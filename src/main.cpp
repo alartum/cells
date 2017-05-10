8572b440de37f8f30fdb4c0b5462bf5e92b9fbcd
Aleksandr Artemenkov <alartum@gmail.com>
2017-05-10 17:12:05 +0300

    Added more wolf textures

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ void UpdateThread(Field& F)
 int test_field(int argc, char** argv, char** env) {
     XInitThreads();
     Field F;
-    F.loadConfig("tileinfo/config.lua");
+    F.loadConfig("./tileinfo/config.lua");
     F.fitView();
 
     GenerateRandomMap mapGenerator(5, 0.01, 0.15, 3);
---
+++ src/tileinfo/test.lua
@@ -0,0 +1,5 @@
+function GameItem(info)
+	print('Here')
+end
+
+dofile('mm_data.lua')
