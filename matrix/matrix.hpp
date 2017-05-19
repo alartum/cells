2791d256aaaf862ffc9b8f0af805c52fd29cfedb
Aleksandr Artemenkov <alartum@gmail.com>
2017-05-19 21:45:49 +0300

    Changed matrix inheritance from vector to private

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -13,32 +13,40 @@
 //! Matrix should BE a vector, not CONTAIN a vector
 //! UPD: Done
 template <typename T>
-class Matrix : public std::vector<T>
+class Matrix : private std::vector<T>
 {
 public:
     Matrix (const Matrix& other);
     Matrix (unsigned height = 0, unsigned width = 0);
     Matrix (unsigned height, unsigned width, const T& element);
     Matrix (unsigned height, unsigned width, T&& element);
-    // Возвращает ссылку элемент с данной позицией
-    T &at(unsigned x, unsigned y);
-    // Заполняет матрицу элементом element
-    inline void fill (const T& element);
-    // Изменяет высоту
-    inline void setHeight (unsigned height);
-    // Изменяет ширину
-    inline void setWidth (unsigned width);
-    // Изменяет оба параметра
-    inline void setSize (unsigned height, unsigned widht);
-    inline unsigned getHeight () const;
-    inline unsigned getWidth () const;
+    T &at(unsigned y, unsigned x);
+    void fill (const T& element);
+    void setHeight (unsigned height);
+    void setWidth (unsigned width);
+    void setSize (unsigned height, unsigned width);
+    unsigned getHeight () const;
+    unsigned getWidth () const;
+
+    // Get iteration abilities from vector
+    using std::vector<T>::begin;
+    using std::vector<T>::end;
+    using std::vector<T>::rbegin;
+    using std::vector<T>::rend;
+    using std::vector<T>::cbegin;
+    using std::vector<T>::cend;
+    using std::vector<T>::crbegin;
+    using std::vector<T>::crend;
+
+    // Get element management from vector
+    using std::vector<T>::clear;
+    using std::vector<T>::swap;
 
 protected:
-    // Корректны ли координаты
-    inline bool isInRange (unsigned x, unsigned y) const;
+    bool isInRange (unsigned x, unsigned y) const;
 private:
     unsigned mHeight, mWidth;
-    
+
     // Оператор вывода в поток
     friend std::ostream & operator << (std::ostream & ostr, Matrix & M)
     {
@@ -52,21 +60,21 @@ private:
         return ostr;
     }
     
-    inline Matrix< T > & operator = (const Matrix< T > Other) const
+    Matrix< T > & operator = (const Matrix< T > Other) const
     {
         std::vector<T>::operator =(Other);
         mWidth = Other.mWidth;
         mHeight = Other.mHeight;
     }
 
-    inline Matrix< T > & operator = (const Matrix< T > & Other) const 
+    Matrix< T > & operator = (const Matrix< T > & Other) const
     {
         std::vector<T>::operator =(Other);
         mWidth = Other.mWidth;
         mHeight = Other.mHeight;
     }
 
-    inline Matrix< T > & operator = (const Matrix< T > && Other) const 
+    Matrix< T > & operator = (const Matrix< T > && Other) const
     {
         std::vector<T>::operator =(Other);
         mWidth = Other.mWidth;
@@ -75,77 +83,77 @@ private:
 };
 
 template <typename T>
-inline Matrix<T>::Matrix(const Matrix& other) :
+Matrix<T>::Matrix(const Matrix& other) :
     std::vector<T>(other), mHeight(other.mHeight), mWidth(other.mWidth)
 {
 }
 
 template <typename T>
-inline Matrix<T>::Matrix (unsigned height, unsigned width):
+Matrix<T>::Matrix (unsigned height, unsigned width):
     std::vector<T>(height * width, T()), mHeight (height), mWidth (width)
 {
 
 }
 
 template <typename T>
-inline Matrix<T>::Matrix (unsigned height, unsigned width, const T &element):
+Matrix<T>::Matrix (unsigned height, unsigned width, const T &element):
     std::vector<T>(height * width, T()), mHeight (height), mWidth (width)
 {
     
 }
 
 template <typename T>
-inline Matrix<T>::Matrix (unsigned height, unsigned width, T&& element):
+Matrix<T>::Matrix (unsigned height, unsigned width, T&& element):
     std::vector<T>(height * width, T()), mHeight (height), mWidth (width)
 {
 
 }
 
 template <typename T>
-inline T &Matrix<T>::at(unsigned x, unsigned y)
+T &Matrix<T>::at(unsigned y, unsigned x)
 {
-    return std::vector<T>::at(x * mWidth + y);
+    return std::vector<T>::at(y * mWidth + x);
 }
 
 
 template <typename T>
-inline bool Matrix<T>::isInRange(unsigned x, unsigned y) const
+bool Matrix<T>::isInRange(unsigned x, unsigned y) const
 {
-   return ((x < mHeight) && (y < mWidth));
+    return ((x < mHeight) && (y < mWidth));
 }
 
 template <typename T>
-inline void Matrix<T>::fill(const T &element)
+void Matrix<T>::fill(const T &element)
 {
     std::fill(std::vector<T>::begin(), std::vector<T>::end(), element);
 }
 
 template <typename T>
-inline unsigned Matrix<T>::getHeight() const
+unsigned Matrix<T>::getHeight() const
 {
     return mHeight;
 }
 
 template <typename T>
-inline unsigned Matrix<T>::getWidth() const
+unsigned Matrix<T>::getWidth() const
 {
     return mWidth;
 }
 
 template <typename T>
-inline void Matrix<T>::setHeight (unsigned height)
+void Matrix<T>::setHeight (unsigned height)
 {
     setSize(height, mWidth);
 }
 
 template <typename T>
-inline void Matrix<T>::setWidth(unsigned width)
+void Matrix<T>::setWidth(unsigned width)
 {
     setSize(mHeight, width);
 }
 
 template <typename T>
-inline void Matrix<T>::setSize(unsigned height, unsigned width)
+void Matrix<T>::setSize(unsigned height, unsigned width)
 {
     if (height == mHeight && width == mWidth)
         return;
